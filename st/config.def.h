b64b8b9617dbc74a91e21c6bbdca3206454403fa
Oliveoilollie <rickeyollie@gmail.com>
2025-08-11 16:19:14 -0500

    scroll

--- st/config.def.h
+++ st/config.def.h
@@ -16,10 +16,10 @@ static int borderpx = 2;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/sh";
+static char *shell = "/bin/bash";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
-char *scroll = NULL;
+char *scroll = "scroll";
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
 /* identification sequence returned in DA and DECID */
@@ -172,10 +172,14 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument      release alt */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},           1 },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\033[5;2~"}, 0, -1 },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\033[6;2~"}, 0, -1 },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	{ ShiftMask,            Button4, zoom,           {.f =  +1} },
+	{ ShiftMask,            Button5, zoom,           {.f =  -1} },
+	{ XK_NO_MOD,            Button4, ttysend,        {.s = "\033[5;2~"}, 0, -1 },
+	{ XK_NO_MOD,            Button4, ttysend,        {.s = "\031"} },
+	{ XK_NO_MOD,            Button5, ttysend,        {.s = "\033[6;2~"}, 0, -1 },
+	{ XK_NO_MOD,            Button5, ttysend,        {.s = "\005"} },
+	{ XK_ANY_MOD,           Button4, zoom,           {.f =  +1} },
+	{ XK_ANY_MOD,           Button5, zoom,           {.f =  -1} },
 };
 
 /* Internal keyboard shortcuts. */
